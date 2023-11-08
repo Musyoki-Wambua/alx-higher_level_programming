a3a2cf0c753ef40e8d803b23512db6ea1c5dfeff
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-11-08 11:10:06 +0300

    Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    This function takes two sets as
+    input and returns a list of elements
+    that are present in both sets.
+    """
+    return(set_1 & set_2)
